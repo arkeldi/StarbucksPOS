6536952ee79b419fef6df6815f2a3ceea9b36e5b
Jack <72028053+Boyifyoudontstop@users.noreply.github.com>
2023-04-04 19:56:08 -0500

    corrected stackblitz upload
    Correctly imported modifications and refactoring from stackblitz into the correct directories in the repository.

---
+++ pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Title from '../components/Title'
+
+const HomePage = props =>
+  <div>
+    <Title>Home Page</Title>
+  </div>
+
+export default styled(HomePage)`
+
+`
