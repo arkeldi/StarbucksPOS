e928122e53bed1f5cc4903b2dafa8b7024156ebf
cade2222 <112445212+cade2222@users.noreply.github.com>
2023-04-18 23:23:59 -0500

    Navbar now stays fixed on screen

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,17 +4,19 @@ import styled from 'styled-components';
 
 const Navbar = ({ className }) => (
   <nav className={className}>
-    <NavLink exact to="/">
-      Home
-    </NavLink>
-    <NavLink to="/about">About</NavLink>
-    <NavLink to="/login">Login</NavLink>
-    <NavLink to="/createaccount">Create Account</NavLink>
-    <NavLink to="/menu">Menu</NavLink>
-    <NavLink to="/product">Product</NavLink>
+    <NavLink exact to="/">Home</NavLink>
+    <NavLink to='AboutPage'>About</NavLink>
+    <NavLink to="Login">Login</NavLink>
+    <NavLink to="createaccount">Create Account</NavLink>
+    <NavLink to="Menu">Menu</NavLink>
+    <NavLink to='Manager'>Manager</NavLink>
   </nav>
-);
+); 
 
 export default styled(Navbar)`
-
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 999;
 `;
