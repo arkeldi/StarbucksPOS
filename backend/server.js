7cd7b1998b7f14a8b0aa86367a4411947bb4d99b
Chenners13 <123605456+Chenners13@users.noreply.github.com>
2023-04-27 20:33:07 -0500

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+
+const pool = new Pool({
+    'host': 'csce-315-db.engr.tamu.edu',
+    'user': 'csce315331_team_62_master',
+    'database': 'csce315331_team_62',
+    'password': 'panda',
+    'port': '5432'
+
+});
+app.get('/cunt', async (req, res) => {
+  try {
+    const data = await pool.query('SELECT * FROM Inventory');
+    res.json(data.rows);
+    console.log(data)
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+});
+app.get('/queryInventory/:start/:end', async (req, res) => {
+    try {
+      const start = parseInt(req.params.start);
+      const end = parseInt(req.params.end);
+      var queryToUse;
+      if((start === 0) && (end === 0)){
+        queryToUse = 'SELECT * FROM inventory ORDER BY id';
+      }
+      else{
+        queryToUse = 'SELECT * FROM inventory WHERE id >= ' + start + ' AND id <= ' + end + ' ORDER BY id';
+      }
+      console.log(queryToUse);
+      const { rows } = await pool.query(queryToUse);
+      res.json(rows);
+      //console.log(rows);
+    } catch (err) {
+
+      console.error("Read query in inventory failed " +err);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+
+// console.log("cunt")
+app.listen(3001, () => {
+  console.log('Server listening on port 5000');
+});
---
+++ backend/server_functions.js
@@ -0,0 +1,163 @@
+import { useState, useEffect } from 'react';
+
+/* Fetch menu items from list to display in table */
+// function GetMenuList(start, end){
+//   const [menuItems, setMenuItems] = useState([]);
+
+//   useEffect(() => {
+//     async function fetchMenuItems() {
+//       const response = await fetch(`http://localhost:3001/menuRequest/${start}/${end}`);
+//       const data = await response.json();
+//       setMenuItems(data);
+//     }
+
+//     fetchMenuItems();
+//   }, [start, end]);
+
+//   console.log(menuItems);
+//   return menuItems;
+// }
+
+/* Fetch menu items from list to display in table */
+function GetInventoryList(start, end){
+  const [inventoryItems, setInventoryItems] = useState([]);
+
+  useEffect(() => {
+    async function fetchInventoryItems() {
+      const response = await fetch(`http://localhost:3001/queryInventory/${start}/${end}`);
+      const data = await response.json();
+      setInventoryItems(data);
+    }
+
+    fetchInventoryItems();
+  }, [start, end]);
+
+  return inventoryItems;
+}
+
+//fetch all menu items from database table
+// function GetAllMenuList(){
+//   const [menuItems, setMenuItems] = useState([]);
+
+//   useEffect(() => {
+//     async function fetchMenuItems() {
+//       const response = await fetch(`http://localhost:3001/menuRequestAll/`);
+//       const data = await response.json();
+//       setMenuItems(data);
+//     }
+
+//     fetchMenuItems();
+//   }, []);
+
+//   return menuItems;
+// }
+
+//Fetch ingredient list to be display in table
+function GetIngredients(start, end) {
+   const [ingredientArr, setIngredientArr] = useState([]);
+
+  useEffect(() => {
+    async function fetchIngredients() {
+      const response = await fetch(`http://localhost:3001/getInventoryItemsForMenu/${start}/${end}`);
+      const data = await response.json();
+      //data.sort((a, b) => a.menu_id - b.menu_id);
+      setIngredientArr(data);
+    }
+
+    fetchIngredients();
+  }, [start, end]);
+
+  return ingredientArr;
+}
+
+let addToCartPromise = Promise.resolve(); //synchronization thing
+function AddToCart(type, name, quantity, price) {
+  const myID = document.cookie.replace(/(?:(?:^|.*;\s*)sessionId\s*=\s*([^;]*).*$)|^.*$/, "$1");
+  //console.log("Adding " +quantity + " units of "+ type + ": " + name+ " to cart for user " + myID + " with price " + price);
+  const item = { type, name, quantity, price };
+
+  addToCartPromise = addToCartPromise.then(async () => {
+    const response = await fetch(`http://localhost:3001/addToCart/${myID}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(item)
+    });
+    const data = await response.json();
+    console.log(data.message);
+  });
+}
+
+
+function GetCartItems(){
+  const [myCart, setCart] = useState([]);
+  var myID = document.cookie.replace(/(?:(?:^|.*;\s*)sessionId\s*=\s*([^;]*).*$)|^.*$/, "$1");
+  //console.log("getting cart");
+
+  useEffect(() => {
+    async function fetchCart() {
+      const response = await fetch(`http://localhost:3001/getCart/${myID}`);
+      const data = await response.json();
+      setCart(data.rows[0].orderlist);
+      //console.log("cart in func: " + data.rows[0].orderlist);
+    }
+    fetchCart();
+  }, [myID]);
+
+  //console.log("cart: " + myCart); // should log an array of cart items
+  const items = [];
+  const modlist = [];
+  var j = 1;
+
+  if(myCart !== null) {
+    for (let i = 0; i < myCart.length; i++) {
+      const element = JSON.parse(myCart[i]);
+      if(element.type === "item") {
+        const item = { id: j, name: element.name, price: element.price, qty: 1, mods: [...modlist]}
+        items.push(item);
+        modlist.length = 0;
+        j++;
+      } else {
+          if(element.name === "Beef Patty") {
+            if(element.quantity === -1)
+              modlist.push("SUB Black Bean Patty");
+          }
+          else if(element.name === "Vanilla Ice Cream") {
+            if(element.quantity === -1)
+              modlist.push("CHOCOLATE");
+            else if(element.quantity === 0)
+              modlist.push("VANILLA");
+            else if(element.quantity === 1)
+              modlist.push("COFFEE");
+            else
+              modlist.push("STRAWBERRY");
+          }
+          else if(element.name === "x") {
+            if(element.quantity === -1)
+              modlist.push("BUFFALO");
+            else if(element.quantity === 0)
+              modlist.push("BBQ");
+            else if(element.quantity === 1)
+              modlist.push("HONEY MUSTARD");
+            else if(element.quantity === 2)
+              modlist.push("RANCH");
+            else if(element.quantity === 3)
+              modlist.push("SPICY RANCH");
+            else if(element.quantity === 4)
+              modlist.push("GIG EM");
+          }
+          else {
+            if(element.quantity === -1)
+              modlist.push("NO " + element.name);
+            else
+              modlist.push("EXTRA " + element.name);
+          }
+      }
+    };
+  }
+  return items;
+}
+
+
+export {GetMenuList, GetIngredients, AddToCart, GetCartItems, GetInventoryList};
