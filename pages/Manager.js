c47ab2bd077c489e197f361c4b2e57ac8301dbfd
Chenners13 <123605456+Chenners13@users.noreply.github.com>
2023-04-29 01:49:36 -0500

    Created changes to the tables that shows a button
    The button does not yet work. I only managed to make the button display in the way I wanted and I also made the outline for function calls, which means that the actual SQL statements for querying and inserting into the database are not done yet.

--- pages/Manager.js
+++ pages/Manager.js
@@ -1,118 +1,51 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import {GetInventoryList} from '../backend/server_functions';
+import {GetInventoryList, GetProductsList, GetCustomizationsList, GetSalesList, GetOrdersList, GetEmployeesList
+  , GetCustomersList, GetRecipesList, GetSizesList, AddItemButton} from '../backend/server_functions';
 
 const Manager = () => {
   const [activeTab, setActiveTab] = useState(0); // Define the active tab state
-  const inventoryData = GetInventoryList(0,0);
+  const [showForm, setShowForm] = useState(false);
 
+
+  const inventoryData = GetInventoryList(0,0);
+  const productData = GetProductsList(0,0);
+  const customizationsData = GetCustomizationsList(0,0);
+  const salesData = GetSalesList(0,0);
+  const ordersData = GetOrdersList(0,0);
+  const employeeData = GetEmployeesList(0,0);
+  const customerData = GetCustomersList(0,0);
+  const recipeData = GetRecipesList(0,0);
+  const sizeData = GetSizesList(0,0);
+
+  //fill in data for everything except transactions
+  // edit function for inventory, products, employees, customers, recipes
+  // x y z tables for the report
   const title = [
     { id: 0, name: 'Inventory', tableData: []
     },
-    { id: 1, name: 'Transactions', tableData: [
-        { id: 1, name: 'Bob', age: 45 },
-        { id: 2, name: 'Alice', age: 36 },
-        { id: 3, name: 'Mark', age: 29 },
-      ] },
-    { id: 2, name: 'Products', tableData: [
-        { id: 1, name: 'Sam', age: 52 },
-        { id: 2, name: 'Sara', age: 31 },
-        { id: 3, name: 'Tom', age: 24 },
-      ] },
-    { id: 3, name: 'Customizations', tableData: [
-      { id: 1, name: 'Sam', age: 52 },
-      { id: 2, name: 'Sara', age: 31 },
-      { id: 3, name: 'Tom', age: 24 },
-    ] },
-    { id: 4, name: 'Sales', tableData: [
-      { id: 1, name: 'Sam', age: 52 },
-      { id: 2, name: 'Sara', age: 31 },
-      { id: 3, name: 'Tom', age: 24 },
-    ] },
-    { id: 5, name: 'Orders', tableData: [
-      { id: 1, name: 'Sam', age: 52 },
-      { id: 2, name: 'Sara', age: 31 },
-      { id: 3, name: 'Tom', age: 24 },
-    ] },
-    { id: 6, name: 'Employees', tableData: [
-      { id: 1, name: 'Sam', age: 52 },
-      { id: 2, name: 'Sara', age: 31 },
-      { id: 3, name: 'Tom', age: 24 },
-    ] },
-    { id: 7, name: 'Customers', tableData: [
-      { id: 1, name: 'Sam', age: 52 },
-      { id: 2, name: 'Sara', age: 31 },
-      { id: 3, name: 'Tom', age: 24 },
-    ] },
-    { id: 8, name: 'Recipes', tableData: [
-      { id: 1, name: 'Sam', age: 52 },
-      { id: 2, name: 'Sara', age: 31 },
-      { id: 3, name: 'Tom', age: 24 },
-    ] },
-    { id: 9, name: 'Sizes', tableData: [
-      { id: 1, name: 'Sam', age: 52 },
-      { id: 2, name: 'Sara', age: 31 },
-      { id: 3, name: 'Tom', age: 24 },
-    ] },
-    { id: 10, name: 'Reports', tableData: [
-      { id: 1, name: 'Sam', age: 52 },
-      { id: 2, name: 'Sara', age: 31 },
-      { id: 3, name: 'Tom', age: 24 },
-    ] },
+    { id: 1, name: 'Transactions', tableData: [] },
+    { id: 2, name: 'Products', tableData: [] },
+    { id: 3, name: 'Customizations', tableData: [] },
+    { id: 4, name: 'Sales', tableData: [] },
+    { id: 5, name: 'Orders', tableData: [] },
+    { id: 6, name: 'Employees', tableData: [] },
+    { id: 7, name: 'Customers', tableData: [] },
+    { id: 8, name: 'Recipes', tableData: [] },
+    { id: 9, name: 'Sizes', tableData: [] },
+    { id: 10, name: 'Reports', tableData: [] },
   ];
   const tabs = [
-    { id: 0, name: 'Inventory', tableData: inventoryData
-      // [ // Define the tabs and their table data
-      //   { id: 1, name: 'John', age: 28, number: "346-530-4989"},
-      //   { id: 2, name: 'Jane', age: 32, number: "346-530-4989" },
-      //   { id: 3, name: 'Joe', age: 21, number: "346-530-4989" },
-      // ] 
-    },
-    { id: 1, name: 'Transactions', tableData: [
-        { id: 1, name: 'Bob', age: 45 },
-        { id: 2, name: 'Alice', age: 36 },
-        { id: 3, name: 'Mark', age: 29 },
-      ] },
-    { id: 2, name: 'Products', tableData: [
-        { id: 1, name: 'Sam', age: 52 },
-        { id: 2, name: 'Sara', age: 31 },
-        { id: 3, name: 'Tom', age: 24 },
-      ] },
-    { id: 3, name: 'Customizations', tableData: [
-      { id: 1, name: 'Sam', age: 52 },
-      { id: 2, name: 'Sara', age: 31 },
-      { id: 3, name: 'Tom', age: 24 },
-    ] },
-    { id: 4, name: 'Sales', tableData: [
-      { id: 1, name: 'Sam', age: 52 },
-      { id: 2, name: 'Sara', age: 31 },
-      { id: 3, name: 'Tom', age: 24 },
-    ] },
-    { id: 5, name: 'Orders', tableData: [
-      { id: 1, name: 'Sam', age: 52 },
-      { id: 2, name: 'Sara', age: 31 },
-      { id: 3, name: 'Tom', age: 24 },
-    ] },
-    { id: 6, name: 'Employees', tableData: [
-      { id: 1, name: 'Sam', age: 52 },
-      { id: 2, name: 'Sara', age: 31 },
-      { id: 3, name: 'Tom', age: 24 },
-    ] },
-    { id: 7, name: 'Customers', tableData: [
-      { id: 1, name: 'Sam', age: 52 },
-      { id: 2, name: 'Sara', age: 31 },
-      { id: 3, name: 'Tom', age: 24 },
-    ] },
-    { id: 8, name: 'Recipes', tableData: [
-      { id: 1, name: 'Sam', age: 52 },
-      { id: 2, name: 'Sara', age: 31 },
-      { id: 3, name: 'Tom', age: 24 },
-    ] },
-    { id: 9, name: 'Sizes', tableData: [
-      { id: 1, name: 'Sam', age: 52 },
-      { id: 2, name: 'Sara', age: 31 },
-      { id: 3, name: 'Tom', age: 24 },
-    ] },
+    { id: 0, name: 'Inventory', tableData: inventoryData },
+    { id: 1, name: 'Transactions', tableData: []},
+    { id: 2, name: 'Products', tableData: productData },
+    { id: 3, name: 'Customizations', tableData: customizationsData },
+    { id: 4, name: 'Sales', tableData: salesData },
+    { id: 5, name: 'Orders', tableData: ordersData },
+    { id: 6, name: 'Employees', tableData: employeeData },
+    { id: 7, name: 'Customers', tableData: customerData },
+    { id: 8, name: 'Recipes', tableData: recipeData },
+    { id: 9, name: 'Sizes', tableData: sizeData },
     { id: 10, name: 'Reports', tableData: [
       { id: 1, name: 'Sam', age: 52 },
       { id: 2, name: 'Sara', age: 31 },
@@ -120,11 +53,25 @@ const Manager = () => {
     ] },
   ];
 
+  function handleSubmit(event) {
+    setShowForm(!showForm);
+    event.preventDefault();
+    // Here, you can send the form data to your Postgres database
+    // using a fetch request or another method
+    console.log("handleSubmitCunt")
+  }
+
+  const handleClick = () => {
+    console.log("formcunt")
+    setShowForm(!showForm);
+  }
   const handleTabClick = (tabIndex) => {
     setActiveTab(tabIndex); // Update the active tab state when a tab is clicked
   };
+  
 
   return (
+      
     <div>
       <div
         style={{
@@ -172,14 +119,6 @@ const Manager = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  {/* {tab.tableData.map((data) => (
-                    <tr key={data.id}>
-                      <td>{data.id}</td>
-                      <td>{data.name}</td>
-                      <td>{data.age}</td>
-                      <td>{data.number}</td>
-                    </tr>
-                  ))} */}
                   {tab.tableData.map((data) => (
                       <tr key={data.id}>
                           {Object.keys(data).map((key) => (
@@ -194,16 +133,57 @@ const Manager = () => {
         </div>
       </div>
       <div
-        style={{
-          backgroundColor: '#1e3932',
-          height: '100px',
-          position: 'fixed',
-          bottom: 0,
-          left: 0,
-          right: 0,
-        }}
-      ></div>
-  </div>)}
+        // style={{
+        //   backgroundColor: '#1e3932',
+        //   height: '100px',
+        //   position: 'fixed',
+        //   bottom: 0,
+        //   left: 0,
+        //   right: 0,
+        // }}
+      >
+      </div>
+
+      <div>
+        {/* <button onClick={handleClick}>Toggle Interface</button> */}
+        {showForm ? (
+          <div>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="id">ID:</label>
+              <input type="text" id="id" name="id" />
+
+              <label htmlFor="itemName">Item Name:</label>
+              <input type="text" id="itemName" name="itemName" />
+
+              <label htmlFor="itemType">Item Type:</label>
+              <input type="text" id="itemType" name="itemType" />
+
+              <label htmlFor="itemQuantity">Item Quantity:</label>
+              <input type="text" id="itemQuantity" name="itemQuantity" />
+
+              <label htmlFor="itemUnit">Item Unit:</label>
+              <input type="text" id="itemUnit" name="itemUnit" />
+
+              <label htmlFor="itemPrice">Item Price:</label>
+              <input type="text" id="itemPrice" name="itemPrice" />
+
+              <label htmlFor="dateOfLastUpdate">Date of Last Update:</label>
+              <input type="text" id="dateOfLastUpdate" name="dateOfLastUpdate" />
+
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        ) : (
+          <div>
+              <button onClick={handleClick}>Add Item</button>
+          </div>
+        )}
+    </div>
+    </div>
+    
+    )
+    
+  }
 
 export default Manager;
 
