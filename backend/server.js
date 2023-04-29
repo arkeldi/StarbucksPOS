66d5c4e21da84e5453ed9e38014ae2f4321c890a
Chenners13 <123605456+Chenners13@users.noreply.github.com>
2023-04-29 01:47:52 -0500

    Created outline for edit functions

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.get('/queryInventory/:start/:end', async (req, res) => {
       const end = parseInt(req.params.end);
       var queryToUse;
       if((start === 0) && (end === 0)){
-        queryToUse = 'SELECT * FROM inventory ORDER BY id';
+        queryToUse = 'SELECT * FROM inventory ORDER BY id limit 10';
       }
       else{
         queryToUse = 'SELECT * FROM inventory WHERE id >= ' + start + ' AND id <= ' + end + ' ORDER BY id';
@@ -43,9 +43,219 @@ app.get('/queryInventory/:start/:end', async (req, res) => {
       console.error("Read query in inventory failed " +err);
       res.status(500).json({ error: 'Internal server error' });
     }
-  });
+});
+app.get('/queryProducts/:start/:end', async (req, res) => {
+  try {
+    const start = parseInt(req.params.start);
+    const end = parseInt(req.params.end);
+    var queryToUse;
+    if((start === 0) && (end === 0)){
+      queryToUse = 'SELECT * FROM products ORDER BY id limit 10';
+    }
+    else{
+      queryToUse = 'SELECT * FROM products WHERE id >= ' + start + ' AND id <= ' + end + ' ORDER BY id';
+    }
+    console.log(queryToUse);
+    const { rows } = await pool.query(queryToUse);
+    res.json(rows);
+    //console.log(rows);
+  } catch (err) {
 
-// console.log("cunt")
+    console.error("Read query in inventory failed " +err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+app.get('/queryCustomizations/:start/:end', async (req, res) => {
+  try {
+    const start = parseInt(req.params.start);
+    const end = parseInt(req.params.end);
+    var queryToUse;
+    if((start === 0) && (end === 0)){
+      queryToUse = 'SELECT * FROM customizations ORDER BY id limit 10';
+    }
+    else{
+      queryToUse = 'SELECT * FROM customizations WHERE id >= ' + start + ' AND id <= ' + end + ' ORDER BY id';
+    }
+    console.log(queryToUse);
+    const { rows } = await pool.query(queryToUse);
+    res.json(rows);
+    //console.log(rows);
+  } catch (err) {
+
+    console.error("Read query in inventory failed " +err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.get('/querySales/:start/:end', async (req, res) => {
+  try {
+    const start = parseInt(req.params.start);
+    const end = parseInt(req.params.end);
+    var queryToUse;
+    if((start === 0) && (end === 0)){
+      queryToUse = 'SELECT * FROM sales ORDER BY id limit 10';
+    }
+    else{
+      queryToUse = 'SELECT * FROM sales WHERE id >= ' + start + ' AND id <= ' + end + ' ORDER BY id';
+    }
+    console.log(queryToUse);
+    const { rows } = await pool.query(queryToUse);
+    res.json(rows);
+    //console.log(rows);
+  } catch (err) {
+
+    console.error("Read query in inventory failed " +err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.get('/queryOrders/:start/:end', async (req, res) => {
+  try {
+    const start = parseInt(req.params.start);
+    const end = parseInt(req.params.end);
+    var queryToUse;
+    if((start === 0) && (end === 0)){
+      queryToUse = 'SELECT * FROM orders ORDER BY sale_id limit 10';
+    }
+    else{
+      queryToUse = 'SELECT * FROM orders WHERE id >= ' + start + ' AND sale_id <= ' + end + ' ORDER BY sale_id';
+    }
+    console.log(queryToUse);
+    const { rows } = await pool.query(queryToUse);
+    res.json(rows);
+    //console.log(rows);
+  } catch (err) {
+
+    console.error("Read query in inventory failed " +err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.get('/queryEmployees/:start/:end', async (req, res) => {
+  try {
+    const start = parseInt(req.params.start);
+    const end = parseInt(req.params.end);
+    var queryToUse;
+    if((start === 0) && (end === 0)){
+      queryToUse = 'SELECT * FROM employees ORDER BY id limit 10';
+    }
+    else{
+      queryToUse = 'SELECT * FROM employees  WHERE id >= ' + start + ' AND id <= ' + end + ' ORDER BY id';
+    }
+    console.log(queryToUse);
+    const { rows } = await pool.query(queryToUse);
+    res.json(rows);
+    //console.log(rows);
+  } catch (err) {
+
+    console.error("Read query in inventory failed " +err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.get('/queryCustomers/:start/:end', async (req, res) => {
+  try {
+    const start = parseInt(req.params.start);
+    const end = parseInt(req.params.end);
+    var queryToUse;
+    if((start === 0) && (end === 0)){
+      queryToUse = 'SELECT * FROM customers ORDER BY id limit 10';
+    }
+    else{
+      queryToUse = 'SELECT * FROM customers WHERE id >= ' + start + ' AND id <= ' + end + ' ORDER BY id';
+    }
+    console.log(queryToUse);
+    const { rows } = await pool.query(queryToUse);
+    res.json(rows);
+    //console.log(rows);
+  } catch (err) {
+
+    console.error("Read query in inventory failed " +err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.get('/queryRecipes/:start/:end', async (req, res) => {
+  try {
+    const start = parseInt(req.params.start);
+    const end = parseInt(req.params.end);
+    var queryToUse;
+    if((start === 0) && (end === 0)){
+      queryToUse = 'SELECT * FROM recipes ORDER BY product_id limit 10';
+    }
+    else{
+      queryToUse = 'SELECT * FROM recipes WHERE product_id >= ' + start + ' AND product_id <= ' + end + ' ORDER BY product_id';
+    }
+    console.log(queryToUse);
+    const { rows } = await pool.query(queryToUse);
+    res.json(rows);
+    //console.log(rows);
+  } catch (err) {
+
+    console.error("Read query in inventory failed " +err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.get('/querySizes/:start/:end', async (req, res) => {
+  try {
+    const start = parseInt(req.params.start);
+    const end = parseInt(req.params.end);
+    var queryToUse;
+    if((start === 0) && (end === 0)){
+      queryToUse = 'SELECT * FROM sizes ORDER BY id limit 10';
+    }
+    else{
+      queryToUse = 'SELECT * FROM sizes WHERE id >= ' + start + ' AND id <= ' + end + ' ORDER BY id';
+    }
+    console.log(queryToUse);
+    const { rows } = await pool.query(queryToUse);
+    res.json(rows);
+    //console.log(rows);
+  } catch (err) {
+
+    console.error("Read query in inventory failed " +err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.get('/queryReports/:start/:end', async (req, res) => {
+  try {
+    const start = parseInt(req.params.start);
+    const end = parseInt(req.params.end);
+    var queryToUse;
+    if((start === 0) && (end === 0)){
+      queryToUse = 'SELECT * FROM reports ORDER BY id limit 10';
+    }
+    else{
+      queryToUse = 'SELECT * FROM reports WHERE id >= ' + start + ' AND id <= ' + end + ' ORDER BY id';
+    }
+    console.log(queryToUse);
+    const { rows } = await pool.query(queryToUse);
+    res.json(rows);
+    //console.log(rows);
+  } catch (err) {
+
+    console.error("Read query in inventory failed " +err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+//not done yet
+app.post('/add-item', async (req, res) => {
+  try {
+    const newItem = req.body; // extract item data from request body
+
+    // Use SQL to insert the new item into the database
+    const query = 'INSERT INTO items (name, description, price) VALUES ($1, $2, $3)';
+    const values = [newItem.name, newItem.description, newItem.price];
+    await pool.query(query, values);
+
+    res.status(200).json({ message: 'Item added successfully' });
+  } catch (err) {
+    console.error('Error adding item to database:', err);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
 app.listen(3001, () => {
   console.log('Server listening on port 5000');
 });
