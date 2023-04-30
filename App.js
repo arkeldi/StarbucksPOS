cf1db00c2f7dbee5d6c5a10ae36cdf794a610b22
Jack <72028053+Boyifyoudontstop@users.noreply.github.com>
2023-04-30 01:48:03 -0500

    implemented OAUTH
    logout and login OUATH added,  app.js changed, and login.js changed. To try OAUTH, give me your email to be added to the testlist

--- App.js
+++ App.js
@@ -1,31 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
-import { Route, Switch } from 'react-router-dom';
+import { Routes ,Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import Login from './pages/Login';
 import CreateAccount from './pages/CreateAccount';
+import PageNotFound from './pages/PageNotFound';
+import Manager from './pages/Manager';
+import Table from './pages/Table';
 import Menu from './pages/Menu';
 import Product from './pages/Product';
-import PageNotFound from './pages/PageNotFound';
+import Order from './pages/Order';
+import LoginButton from './components/loginOAUTH';
+import LogoutButton from './components/logoutOAUTH';
+import {gapi} from 'gapi-script';
 
-const App = ({ className }) => (
-  <div className={className}>
-    <Navbar />
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/about" component={AboutPage} />
-      <Route path="/login" component={Login} />
-      <Route path="/createaccount" component={CreateAccount} />
-      <Route path="/menu" component={Menu} />
-      <Route path="/product" component={Product} />
-      <Route component={PageNotFound} />
-    </Switch>
-  </div>
-);
+const clientId = "1084493453935-kmj0oh3e6kl1dtintpt3kr74q3seutme.apps.googleusercontent.com";
+
+function App() {
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId: clientId,
+        scope: "",
+      });
+    }
+    gapi.load('client:auth2', start);
+  }, []);
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/AboutPage" element={<AboutPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/createaccount" element={<CreateAccount />} />
+        <Route path="/manager" element={<Manager />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/table" element={<Table />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/order" element={<Order />} />
+        <Route element={<PageNotFound />} />
+      </Routes>
+      <div className="App1">
+        <LoginButton />
+        <LogoutButton />
+      </div>
+    </div>
+  );
+}
 
 export default styled(App)`
-
 `;
